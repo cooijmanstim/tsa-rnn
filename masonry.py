dbaa760eae28fdddfd12976d26c022ff5a6e9a18
Tim Cooijmans <cooijmans.tim@gmail.com>
2015-07-08 14:48:26 -0400

    allow zooming in from shrink-to-fit only

--- masonry.py
+++ masonry.py
@@ -81,15 +81,14 @@ class Locator(Initializable):
 def static_map_to_image_space(location, scale, patch_shape, image_shape):
     # linearly map locations from (-1, 1) to image index space
     location = (location + 1) / 2 * image_shape
-    # take exp(scale) to ensure it is positive
-    scale = T.exp(scale) if isinstance(scale, T.TensorVariable) else np.exp(scale)
-    # multiply scale such that scale = 1 corresponds to shrinking
-    # the full image to fit into the patch.  i.e. by default the
-    # model looks at a very coarse version of the image, and can
-    # choose to selectively refine regions
-    scale *= patch_shape / image_shape
-    # avoid divisions by zero
-    scale += 1e-8
+    # disallow negative scale
+    scale *= scale > 0
+    # translate scale such that scale = 0 corresponds to shrinking the
+    # full image to fit into the patch, and the model can only zoom in
+    # beyond that.  i.e. by default the model looks at a very coarse
+    # version of the image, and can choose to selectively refine
+    # regions
+    scale += patch_shape / image_shape
     return location, scale
 
 class SpatialAttention(Initializable):
@@ -111,8 +110,7 @@ class SpatialAttention(Initializable):
     def compute_initial_location_scale(self, x):
         location = T.alloc(T.cast(0.0, floatX),
                            x.shape[0], self.cropper.n_spatial_dims)
-        # shrink the image to fit in the patch exactly
-        scale = T.ones_like(location) * patch_shape / image_shape
+        scale = T.zeros_like(location)
         return location, scale
 
     @application(inputs=['x', 'h'], outputs=['u', 'location', 'scale', 'patch', 'mean_savings'])
