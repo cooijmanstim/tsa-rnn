5e4d4da7d6b9f5ec9d053ff11039f5b297d1d1bc
Tim Cooijmans <cooijmans.tim@gmail.com>
2015-09-25 17:02:05 -0400

    kth human activity recognition task

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from patchmonitor import PatchMonitoring, VideoPatchMonitoring
 
 import mnist
 import cluttered_mnist_video
+import kth
 import svhn
 import goodfellow_svhn
 
@@ -39,6 +40,7 @@ floatX = theano.config.floatX
 def get_task(task_name, hyperparameters, **kwargs):
     klass = dict(mnist=mnist.Task,
                  cluttered_mnist_video=cluttered_mnist_video.Task,
+                 kth=kth.Task,
                  svhn_digit=svhn.DigitTask,
                  svhn_number=goodfellow_svhn.NumberTask)[task_name]
     return klass(**hyperparameters)
